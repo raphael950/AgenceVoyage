68031baaec9196a1cc341f73c51d4a182ef73ca6
Raphaël <twizox@gmail.com>
2025-03-20 20:03:21 +0100

    chore: add comments

--- edit_trigger.php
+++ edit_trigger.php
@@ -13,7 +13,11 @@
     foreach ($users as &$user) {
         if (!isset($user["email"]) || $user["email"] != $_SESSION[ "user"]["email"]) continue;
 
+        // User is found in the json
+
         $user["username"] = $nom;
+
+        // TODO: check if the new email is not already in use
         $user["email"] = $email;
         $user["gender"] = $gender;
         $user["birth"] = $date;
