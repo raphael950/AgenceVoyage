5ee5f7358073beff4f57b7998d13a472c8e7a8cd
Raphaël <twizox@gmail.com>
2025-04-02 19:47:21 +0200

    feat: total price and get travel utils

--- include/voyage_utils.php
+++ include/voyage_utils.php
@@ -7,5 +7,34 @@
         }
         return $jours;
     }
+    
+    function voyageFromId($id) {
+        $voyages = json_decode(file_get_contents('data/voyages.json'), true);
+        $voy;
+        foreach ($voyages as $v) {
+            if ($v[id] == $id) {
+                $voy = $v;
+                break;
+            }
+        }
+        return voy;
+    }
+
+    function prixTotal($reserv) {
+        $options_select = $reserv["options_souscrites"];
+        $voyage = voyageFromId($reserv["voyage_id"]);
+        $prix_base = $voyage["prix"] * $reserv["nombre_personne"];
+
+        $prix_options = 0;
+        foreach ($voyage['etapes'] as $etape) {
+            foreach ($etape['options'] as $option) {
+                // Vérifier si l'option a été sélectionnée
+                if (in_array($option["nom"], array_keys($options_select))) {
+                    $prix_options += $option["prix"] * $quantite_option;
+                }
+            }
+        }
+        return $prix_base + $prix_options;
+    }
 
 ?>
